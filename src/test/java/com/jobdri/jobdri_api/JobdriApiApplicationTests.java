9ab6ffd979725a8f27af45298c4f6ff82961083e
wooh <whc3377@gmail.com>
2026-04-30 18:24:21 +0900

    [Feat] Spring Boot project setup with H2, Gradle, and base config

---
+++ src/test/java/com/jobdri/jobdri_api/JobdriApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobdri.jobdri_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobdriApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
