4c0f09189c8f16e2af2e2bd6721d96388241b59e
shinae1023 <shinae1023@naver.com>
2026-07-04 12:27:19 +0900

    [Refactor] job posting ingest 비동기 상태 저장을 DB 기반으로 전환 (#110)

--- src/main/java/com/jobdri/jobdri_api/domain/jobposting/dto/response/JobPostingIngestResponse.java
+++ src/main/java/com/jobdri/jobdri_api/domain/jobposting/dto/response/JobPostingIngestResponse.java
@@ -2,10 +2,12 @@ package com.jobdri.jobdri_api.domain.jobposting.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class JobPostingIngestResponse {
 
